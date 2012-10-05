276d13b74c47b259f0871a3b28580c20c16d2f8c
Pawel Bedynski <p.bedynski.mobile@gmail.com>
2012-10-05 16:57:34 +0100

    Cleaning projects from unwanted classes, move to java 1.6, lookupgroups configured

--- openspace-clients/src/com/ubs/openspace/client/KPIFeeder.java
+++ openspace-clients/src/com/ubs/openspace/client/KPIFeeder.java
@@ -6,35 +6,46 @@ import org.openspaces.core.space.UrlSpaceConfigurer;
 
 import com.ubs.openspace.model.KPI;
 
-
 public class KPIFeeder {
 
-	
+	private static final long SLEEP_TIME = 10000;
+	private static final boolean SHOULD_STOP = false;
+	private static final int KPIS_PER_ITERATION = 5;
+
 	private final static String SPACE_CONF = "jini://*/*/OS";
-	
+
 	protected GigaSpace gigaSpace;
-	
-	protected void connect() throws Exception{
-		if (gigaSpace == null){
-			gigaSpace = new GigaSpaceConfigurer(new UrlSpaceConfigurer(SPACE_CONF).space()).gigaSpace();
+
+	protected void connect() throws Exception {
+		if (gigaSpace == null) {
+			gigaSpace = new GigaSpaceConfigurer(new UrlSpaceConfigurer(
+					SPACE_CONF).lookupGroups(System.getenv("LOOKUPGROUPS"))
+					.space()).gigaSpace();
 		}
+
 	}
-	
+
 	public void feed() throws InterruptedException {
-		int n=0;
-		while(true){
-			int i=5;
-			while (i-- >0) {
-				KPI kpi = new KPI(); kpi.setName("kpi"+n++); kpi.setValue(n);
+		int kpiCounter = 0;
+		while (!SHOULD_STOP || kpiCounter < 100) {
+			int i = KPIS_PER_ITERATION;
+			while (i-- > 0) {
+				KPI kpi = new KPI();
+				kpi.setName("KPI_" + kpiCounter++);
+				kpi.setValue(kpiCounter);
 				gigaSpace.write(kpi);
 			}
-			Thread.sleep(10000);
+			System.out.println(String.format(
+					"Inserted %d new KPIs, sleeping (%d s)",
+					KPIS_PER_ITERATION, SLEEP_TIME));
+			Thread.sleep(SLEEP_TIME);
 		}
 	}
-	
+
 	public static void main(String[] s) throws Exception {
+		System.out.println("Lookupgroups: " + System.getenv("LOOKUPGROUPS"));
 		KPIFeeder feeder = new KPIFeeder();
- 		feeder.connect();
+		feeder.connect();
 		feeder.feed();
 	}
 
--- openspace-space/src/com/ubs/openspace/model/KPI.java
+++ openspace-space/src/com/ubs/openspace/model/KPI.java
@@ -2,20 +2,28 @@ package com.ubs.openspace.model;
 
 import java.io.Serializable;
 
+import com.gigaspaces.annotation.pojo.SpaceClass;
+
+@SpaceClass
 public class KPI implements Serializable {
-	
+
 	private static final long serialVersionUID = -5725663251414957522L;
-	String name;
-	Integer value;
+
+	private String name;
+	private Integer value;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Integer getValue() {
 		return value;
 	}
+
 	public void setValue(Integer value) {
 		this.value = value;
 	}
